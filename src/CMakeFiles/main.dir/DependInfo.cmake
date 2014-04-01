fd1c9caa738e54370029f9472a59a978535678d8
ocuadrosl <ocuadrosl@gmail.com>
2014-03-31 22:17:07 -0300

    add cmakefile and some eclipse configurations

---
+++ src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/oscar/Documentos/universidad/usp-icmc/doctorado/src/super_voxels/src/super_voxels.cpp" "/home/oscar/Documentos/universidad/usp-icmc/doctorado/src/super_voxels/src/CMakeFiles/main.dir/super_voxels.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ITK_IO_FACTORY_REGISTER_MANAGER"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "ITKIOFactoryRegistration"
+  "/usr/local/include/ITK-4.5"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- src/super_voxels.cpp
+++ src/super_voxels.cpp
@@ -6,10 +6,19 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include "itkImage.h"
+#include "itkImageFileWriter.h"
+#include "itkImageSeriesReader.h"
+
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+	typedef signed short input_pixel_t;
+	typedef itk::Image<input_pixel_t, 3> image_t;
+	typedef itk::ImageFileReader<image_t> reader_t;
+
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
